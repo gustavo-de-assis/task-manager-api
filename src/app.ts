d1cee73fcd4abf0e0c0e403dcbe27bbd41da482d
gustavo de assis <gustavo.siqueira.421@gmail.com>
2024-04-19 11:16:41 -0300

    feat: initial configuration files, typescript and lint

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Express } from "express";
+import cors from "cors";
+
+const app = express();
+
+app
+  .use(cors())
+  .use(express.json())
+  .get("/health", (_req, res) => res.send("OK!"));
+
+export default app;
