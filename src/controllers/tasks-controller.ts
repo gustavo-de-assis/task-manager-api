f29cb4db8093965f6c942ee16d22e2a3710e792b
gustavo de assis <gustavo.siqueira.421@gmail.com>
2024-12-12 17:21:57 -0300

    refact: remove console log when adding tasks

--- src/controllers/tasks-controller.ts
+++ src/controllers/tasks-controller.ts
@@ -9,7 +9,6 @@ export async function postTask(req: Request, res: Response) {
 
     return res.sendStatus(httpStatus.OK);
   } catch (error) {
-    console.log(error);
     return res.status(httpStatus.BAD_REQUEST).send(error.message);
   }
 }
